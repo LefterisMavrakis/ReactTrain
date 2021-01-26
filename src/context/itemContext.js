0770a17ebb83a5b9d055e1e1c4df1db34f937813
LefterisMavrakis <58477560+LefterisMavrakis@users.noreply.github.com>
2021-01-26 11:12:08 +0200

    Impovements

--- src/context/itemContext.js
+++ src/context/itemContext.js
@@ -4,15 +4,6 @@ export const ItemContext = createContext([])
 
 export function ItemContextProvider(props) {
     const [items, setItems] = useState([])
-    const fetchItems = async () => {
-        const response = await axios.get('https://fortnite-api.theapinetwork.com/store/get')
-        const result = await response.data.data
-        setItems(result)
-    }
-
-    useEffect(() => {
-        fetchItems()
-    }, [])
 
     return (
         <ItemContext.Provider value={[items, setItems]}>
